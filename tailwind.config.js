0868f73a1a238a6c3a471a4e303c832c9f984915
Giovana <23111728@CFP127ALUNO.LOCAL>
2024-03-05 17:00:34 -0300

    começo_front

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: { 
+      vermelho: '#9F2323',
+      vermelho2: '#550F0F'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
